6baca38abf5e3259ebada0b038a758f66509c308
Daniel Bickett <dbickett@gmail.com>
2017-08-17 17:59:24 +0200

    k means working; strip out line sets which include column qties deviant from the average column qty for that subject

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
+# from dotenv import load_dotenv, find_dotenv
+# load_dotenv(find_dotenv())
 
-from panoptes_client import Project, Panoptes, Workflow, SubjectSet, Classification
-from settings import *
-import json
+# from panoptes_client import Project, Panoptes, Workflow, SubjectSet, Classification
+# from settings import *
 
 # Panoptes.connect(username=PANOPTES_USERNAME, password=PANOPTES_PASSWORD)
 
 # project = Project.find(slug=PROJECT_SLUG)
 
+import json
+from numpy import array, mean
+from scipy.cluster.vq import vq, kmeans, whiten
+
 with open('mock_annotations.json', 'r') as json_file:
   mock_annotations = json.load(json_file)
-  guesses_by_subject = {}
-  aggregate_vertical_lines_by_subject = {}
-  for subject_annotation in mock_annotations:
-    annotations, subject_id = subject_annotation
-    if not subject_id in aggregate_vertical_lines_by_subject:
-      aggregate_vertical_lines_by_subject[subject_id] = []
-    column_annotations = [a for a in annotations if a['task'] == 'T1']
-    for column_annotation in column_annotations:
-      # Take length of first vertical line classification as k-means 'guess'
-      if not subject_id in guesses_by_subject:
-        guesses_by_subject[subject_id] = len(column_annotation['value'])
-      for vertical_line in column_annotation['value']:
-        aggregate_vertical_lines_by_subject[subject_id].append(vertical_line['x'])
-  for subject_id in aggregate_vertical_lines_by_subject.keys():
-    aggregate_vertical_lines_by_subject[subject_id].sort()
-  import pdb; pdb.set_trace();
+
+line_sets_by_subject = {}
+
+for subject_annotation in mock_annotations:
+  annotations, subject_id = subject_annotation
+  if not subject_id in line_sets_by_subject:
+    line_sets_by_subject[subject_id] = []
+  column_annotations = [a for a in annotations if a['task'] == 'T1']
+
+  # Aggregate line sets by subject
+  for column_annotation in column_annotations:
+    # Append classification line set to subject's sets
+    classification_line_set = [line['x'] for line in column_annotation['value']]
+    line_sets_by_subject[subject_id].append(classification_line_set)
+
+  # Strip out line sets which include column qties deviant from the average column qty for that subject
+  # This works in our case because the quantity of columns on a subject is extremely clear and we simply want to
+  #   exclude anomalous / erroneous entries, which should be very rare, so that they don't ruin our k means logic.
+  for subject_id, line_sets in line_sets_by_subject.items():
+    avg_column_qty =  mean([len(line_set) for line_set in line_sets])
+    line_sets_by_subject[subject_id] = [line_set for line_set in line_sets_by_subject[subject_id] if len(line_set) == avg_column_qty]
+
+k_means_by_subject = {}
+
+for subject_id, line_sets in line_sets_by_subject.items():
+  features = array(line_sets)
+  whitened = whiten(features)
+  book = array((whitened[0],whitened[2]))
+  k_means_by_subject[subject_id] = kmeans(whitened,book)
+
+import pdb; pdb.set_trace();
