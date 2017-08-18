b8a174d3dbbd33dcc54425c549272861be09ffd9
Daniel Bickett <dbickett@gmail.com>
2017-08-18 12:53:08 +0200

    kmeans calculations on api fetched classification data

--- main.py
+++ main.py
@@ -9,32 +9,32 @@ from scipy.cluster.vq import kmeans, whiten
 
 import logging
 
-class Main:
+class Runner:
 
   def __init__(self, log_level = logging.DEBUG):
     self._setupLogger(log_level)
 
   def _setupLogger(self, log_level):
-    _logger = logging.getLogger('WiresRailsWorkflowProcessor')
-    _logger.setLevel(logging.DEBUG)
+    logger = logging.getLogger('WiresRailsWorkflowProcessor')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
     fileHandler = logging.FileHandler('run.log')
     fileHandler.setLevel(log_level)
+    fileHandler.setFormatter(formatter)
 
     consoleHandler = logging.StreamHandler()
     consoleHandler.setLevel(log_level)
-
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fileHandler.setFormatter(formatter)
     consoleHandler.setFormatter(formatter)
 
-    _logger.addHandler(fileHandler)
-    _logger.addHandler(consoleHandler)
+    logger.addHandler(fileHandler)
+    logger.addHandler(consoleHandler)
 
   def run(self):
     Panoptes.connect(username=PANOPTES_USERNAME, password=PANOPTES_PASSWORD)
+
     processor = ClusterAndCropRawPagesByColumnAnnotations()
-    processor.run()
+    processor.processAndCropCompletedSubjects()
 
 class ClusterAndCropRawPagesByColumnAnnotations:
 
@@ -44,16 +44,19 @@ class ClusterAndCropRawPagesByColumnAnnotations:
     self._subjectSetId = 8339 # pages_raw
     self._taskId = 'T1' # Only column demarcation task
 
-    # Populated by _loadData
-    self._classificationsByTaskAndSubject = {}
+    # Populated by _loadData()
+    self._annotationsByTaskAndSubject = {}
     self._classifications = []
     self._subjectSet = None
     self._subjects = []
     self._subjectIds = []
 
+    # Populated by processAndCropCompletedSubjects()
+    self._column_annotations_by_subject = {}
+    self._kmeans_by_subject = {}
+
     self._loadData()
     self._structureClassificationData()
-    import pdb; pdb.set_trace();
 
   def _loadData(self):
     self._loadWorkflowData()
@@ -65,7 +68,7 @@ class ClusterAndCropRawPagesByColumnAnnotations:
     self._workflow = Workflow.find(self._workflowId)
 
   """
-  self._classificationsByTaskAndSubject[taskId][subjectId] = taskAnnotation
+  self._annotationsByTaskAndSubject[taskId][subjectId] = taskAnnotation
   """
   def _structureClassificationData(self):
     self._logger.debug("Structuring classifications")
@@ -73,11 +76,11 @@ class ClusterAndCropRawPagesByColumnAnnotations:
       for subjectId in c.raw['links']['subjects']:
         for taskAnnotation in c.raw['annotations']:
           taskId = taskAnnotation['task']
-          if taskId not in self._classificationsByTaskAndSubject:
-            self._classificationsByTaskAndSubject[taskId] = {}
-          if subjectId not in self._classificationsByTaskAndSubject[taskId]:
-            self._classificationsByTaskAndSubject[taskId][subjectId] = []
-          self._classificationsByTaskAndSubject[taskId][subjectId].append(taskAnnotation)
+          if taskId not in self._annotationsByTaskAndSubject:
+            self._annotationsByTaskAndSubject[taskId] = {}
+          if subjectId not in self._annotationsByTaskAndSubject[taskId]:
+            self._annotationsByTaskAndSubject[taskId][subjectId] = []
+          self._annotationsByTaskAndSubject[taskId][subjectId].append(taskAnnotation)
 
   # TODO only pull subjects which haven't been retired / completed
   def _loadClassificationData(self):
@@ -95,49 +98,72 @@ class ClusterAndCropRawPagesByColumnAnnotations:
     self._subjects = [s for s in self._subjectSet.subjects]
     self._subjectIds = [s.id for s in self._subjects]
 
-  def run(self):
-    pass
-
   def processAndCropCompletedSubjects(self):
+    self._collectColumnSetAnnotationsBySubject()
+    self._normalizeColumnSetAnnotations()
+    import pdb; pdb.set_trace();
+    self._calculateKMeansOfColumnSetAnnotations()
+    import pdb; pdb.set_trace();
+
+  def _collectColumnSetAnnotationsBySubject(self):
+    column_annotations = self._annotationsByTaskAndSubject[self._taskId]
+    for subject_id, annotations in column_annotations.items():
+      if not subject_id in self._column_annotations_by_subject:
+        self._column_annotations_by_subject[subject_id] = []
+      for annotation in annotations:
+        column_vertices = [line['x'] for line in annotation['value']]
+        self._column_annotations_by_subject[subject_id].append(column_vertices)
+
+  """
+  Strip out line sets which include column qties deviant from the average column qty for that subject
+  This works in our case because the quantity of columns on a subject is extremely clear and we simply want to
+    exclude anomalous / erroneous entries, which should be very rare, so that they don't ruin our k means logic.
+  """
+  def _normalizeColumnSetAnnotations(self):
+    for subject_id, line_sets in self._column_annotations_by_subject.items():
+      avg_column_qty = round(mean([len(line_set) for line_set in line_sets]))
+      import pdb; pdb.set_trace();
+      normalized_set = [line_set for line_set in self._column_annotations_by_subject[subject_id] if len(line_set) == avg_column_qty]
+      import pdb; pdb.set_trace();
+      self._column_annotations_by_subject[subject_id] = normalized_set
+
+    # import pdb; pdb.set_trace();
+    # line_sets_by_subject = {}
+
     # with open('mock_annotations.json', 'r') as json_file:
     #   mock_annotations = json.load(json_file)
 
-    line_sets_by_subject = {}
+    # for subject_annotation in mock_annotations:
+    #   annotations, subject_id = subject_annotation
+    #   if not subject_id in line_sets_by_subject:
+    #     line_sets_by_subject[subject_id] = []
+    #   column_annotations = [a for a in annotations if a['task'] == self._taskId]
 
-    for subject_annotation in mock_annotations:
-      annotations, subject_id = subject_annotation
-      if not subject_id in line_sets_by_subject:
-        line_sets_by_subject[subject_id] = []
-      column_annotations = [a for a in annotations if a['task'] == 'T1']
+    #   # Aggregate line sets by subject
+    #   for column_annotation in column_annotations:
+    #     # Append classification line set to subject's sets
+    #     classification_line_set = [line['x'] for line in column_annotation['value']]
+    #     line_sets_by_subject[subject_id].append(classification_line_set)
 
-      # Aggregate line sets by subject
-      for column_annotation in column_annotations:
-        # Append classification line set to subject's sets
-        classification_line_set = [line['x'] for line in column_annotation['value']]
-        line_sets_by_subject[subject_id].append(classification_line_set)
+    #   # Strip out line sets which include column qties deviant from the average column qty for that subject
+    #   # This works in our case because the quantity of columns on a subject is extremely clear and we simply want to
+    #   #   exclude anomalous / erroneous entries, which should be very rare, so that they don't ruin our k means logic.
+    #   for subject_id, line_sets in line_sets_by_subject.items():
+    #     avg_column_qty = round(mean([len(line_set) for line_set in line_sets]))
+    #     line_sets_by_subject[subject_id] = [line_set for line_set in line_sets_by_subject[subject_id] if len(line_set) == avg_column_qty]
 
-      # Strip out line sets which include column qties deviant from the average column qty for that subject
-      # This works in our case because the quantity of columns on a subject is extremely clear and we simply want to
-      #   exclude anomalous / erroneous entries, which should be very rare, so that they don't ruin our k means logic.
-      for subject_id, line_sets in line_sets_by_subject.items():
-        avg_column_qty = round(mean([len(line_set) for line_set in line_sets]))
-        line_sets_by_subject[subject_id] = [line_set for line_set in line_sets_by_subject[subject_id] if len(line_set) == avg_column_qty]
-
-    kmeans_by_subject = {}
-
+  def _calculateKMeansOfColumnSetAnnotations(self):
     # Calculate k means
-    for subject_id, line_sets in line_sets_by_subject.items():
+    for subject_id, line_sets in self._column_annotations_by_subject.items():
       features = array(line_sets)
       std_dev = std(features, axis=0)
       whitened = whiten(features)
       kmeans_codebook, distortion = kmeans(whitened, 1)
-      kmeans_by_subject[subject_id] = kmeans_codebook * std_dev
+      self._kmeans_by_subject[subject_id] = kmeans_codebook * std_dev
       # book = array((whitened[0],whitened[2]))
       # kmeans_codebook, distortion = kmeans(whitened, book)
-      # kmeans_by_subject[subject_id] = (kmeans_codebook * std_dev)[-1]
-
-    import pdb; pdb.set_trace();
+      # self._kmeans_by_subject[subject_id] = (kmeans_codebook * std_dev)[-1]
 
 if __name__ == '__main__':
-  main = Main()
-  main.run()
+  runner = Runner()
+  runner.run()
