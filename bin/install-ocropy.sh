669c5a368121c49d016f3cc523a21b00625d7dab
Daniel Bickett <dbickett@gmail.com>
2017-09-08 13:38:38 +0200

    speed up docker build, add en-default.pyrnn.gz to clone

--- bin/install-ocropy.sh
+++ bin/install-ocropy.sh
@@ -4,8 +4,7 @@ apt-get -y install wget python-tk
 
 git clone https://github.com/tmbdev/ocropy.git
 pushd ocropy
-    wget -nd http://www.tmbdev.net/en-default.pyrnn.gz
-    mv en-default.pyrnn.gz models/
+    mv /app/en-default.pyrnn.gz models/
     python setup.py install
     pip install -r requirements.txt
 popd
