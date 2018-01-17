60efb1248fc5e7cdb7823b34a76589f954a1e90b
freen <dbickett@gmail.com>
2018-01-17 08:02:25 +0100

    docstrings, SubjectSetCSV

--- lib/subject_set_csv.py
+++ lib/subject_set_csv.py
@@ -1,10 +1,25 @@
+"""
+We reduce unnecessary calls to the Panoptes API by using the project's base subject export CSV
+to answer as many questions as possible, namely where the stable "pages_raw" subjects are
+concerned. This CSV is exported from the project backend [1] and must be located in the
+application's docker container at the path: settings.CACHE_FILE_SUBJECT_SET_CSV_FILEPATH
+
+[1] https://www.zooniverse.org/lab/3370/data-exports
+"""
+
 import csv
 import os.path
 
 from lib import settings
 
-# TODO singleton
 class SubjectSetCSV:
+    """
+    Reduce unnecessary calls to the Panoptes API by using the project's base subject export
+    CSV to answer as many questions as possible, namely where the stable "pages_raw" subjects
+    are concerned.
+
+    TODO singleton
+    """
 
     def __init__(self):
         csv_filepath = settings.CACHE_FILE_SUBJECT_SET_CSV_FILEPATH
@@ -17,25 +32,34 @@ class SubjectSetCSV:
         self._subject_ids_by_subject_set_id = None
 
     def raw_pages_subject_ids(self):
+        """
+        Returns a set of subject ids of all raw pages, both Telegraph and Railroad.
+        """
         if self._raw_pages_subject_ids is None:
             ids = [row['subject_id'] for row in self.csv_reader \
                 if int(row['subject_set_id']) in settings.SUBJECT_SET_IDS_PAGES_RAW]
             self._raw_pages_subject_ids = set(ids)
         return self._raw_pages_subject_ids
 
-    # WARN not useful for subjects which are in more than one subject set (unlike raw pages)
     def get_subject_set_id(self, subject_id):
+        """
+        Returns the subject set ID which corresponds to a subject which is only in one subject
+        set, namely any raw page subject.
+        """
         for subject_set_id, subject_ids in self.subject_ids_by_subject_set_id().items():
             if int(subject_id) in subject_ids:
                 return subject_set_id
         return False
 
     def subject_ids_by_subject_set_id(self):
+        """
+        2D array grouping subject IDs into their respective subject set IDs.
+        """
         if self._subject_ids_by_subject_set_id is None:
             self._subject_ids_by_subject_set_id = {}
             for row in self.csv_reader:
                 set_id = int(row['subject_set_id'])
-                if not set_id in self._subject_ids_by_subject_set_id:
+                if set_id not in self._subject_ids_by_subject_set_id:
                     self._subject_ids_by_subject_set_id[set_id] = set()
                 self._subject_ids_by_subject_set_id[set_id].add(int(row['subject_id']))
         return self._subject_ids_by_subject_set_id
